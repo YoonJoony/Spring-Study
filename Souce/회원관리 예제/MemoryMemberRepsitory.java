182c5643b42c3f3ad03fb20258969b172a9cd219
YoonJoony <110625854+YoonJoony@users.noreply.github.com>
2022-08-09 19:41:54 +0900

    Update MemoryMemberRepsitory.java

--- Souce/회원관리 예제/MemoryMemberRepsitory.java
+++ Souce/회원관리 예제/MemoryMemberRepsitory.java
@@ -11,34 +11,38 @@ public class MemoryMemberRepository implements MemberRepository{
 
     //save할 때 메모리를 저장을 할 때
     private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 0L;
+    private static long sequence = 0L; //L은 Long 형식이라는 뜻. Long 형식과 비교할 때 0L사용(id 형시
     //회원이 Long, 값은 Member
 
     @Override
     public Member save(Member member) {
         member.setId(++sequence); //member id값 셋팅
-        store.put(member.getId(), member); //
+        store.put(member.getId(), member); //map 형식 컬렉션으로 만든 store 공간에 키, 값 추가
         return member;
     }
 
     @Override
-    public Optional<Member> findById(Long id) { //id 찾기
+    public Optional<Member> findById(Long id) { //id 찾기(지만 객체 형식으로 반환됨)
         return Optional.ofNullable(store.get(id));
-        //store에서 save에 입력받은 id를 꺼낸다.
-        //그냥 store.get(id)로 반환해도 되지만 Optional로 반환 타입이 명시되어 있고
+        //store에서 save에 입력받은 id인 키에 해당하는 값(value)을 꺼낸다 => id에 해당하는 member객체 반환됨.
+        //그냥 store.get(id)로 반환해도 되지만 interface 선언부에 Optional<memver>로 반환 타입이 명시되어 있고
         //꺼낸 id가 널 값일 수 있으므로 요즘에는 널 값을 꺼낼 시 Optional로 감싸서 꺼낸다.
     }
 
     @Override
-    public Optional<Member> findByName(String name) {
-        return store.values().stream()
-                .filter(member -> member.getName().equals(name)) //String으로 넘어온 name이 member에 저장된 name과 같은지 확인
-                .findAny(); //map에서 하나 찾으면 바로 반환
+    public Optional<Member> findByName(String name) { //String name에 해당하는 객체 반환
+        return store.values().stream() //store의 값들을 흐름(stream)에 따라 연산
+                .filter(member -> member.getName().equals(name)) //중간 연산 : store의 첫 번째로 넘어온 값(member)에 해당하는 name이 String name과 같은지 filter로 거름.
+                .findAny(); //store에서 하나 찾으면 바로 반환
                             //없으면 Optional에 널이 감싸져서 반환
     }
 
     @Override
-    public List<Member> findAll() {  //자바 실무할 떄는 List 많이 쓴다
-        return new ArrayList<>(store.values()); //store의 member 전부 반환
+    public List<Member> findAll() {  //List 컬렉션 형식으로 객체들 반환됨
+        return new ArrayList<>(store.values()); //store의 값(member)들 전부 List 형식으로 공간에 반환됨.
+    }
+
+    public void clearStore() {
+        store.clear(); //store 싹 비우기
     }
 }
